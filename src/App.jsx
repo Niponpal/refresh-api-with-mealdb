9cb90b28077a8ac740483c95f2eb3fb28c03104d
pobittra chandra roy <devpobittra@gmail.com>
2026-03-16 19:40:59 +0600

    create Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <div>
         <Navbar></Navbar>
+        <Hero></Hero>
       </div>
     </>
   )
