765e29aba46c995b586e6f99b49f39c0316ae5b9
pobittra chandra roy <devpobittra@gmail.com>
2026-06-22 21:33:33 +0600

    change navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                             tabIndex="-1"
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                             <li><a>Home</a></li>
-                            <li><a>About</a></li>
+                            <li><a>Aboutc change</a></li>
                             <li><a>Place Order</a></li>
                         </ul>
                     </div>
