d7d7bbcff11c7a39a3774efb0bc6d89e6a667443
niponpal <niponpal52@gmail.com>
2026-06-30 21:22:17 +0600

    rewrite the button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -20,7 +20,7 @@ const Card = ({ image, title, description, price }) => {
             ${price}
           </span>
           <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
-            Buy Now
+            Buy Now Here
           </button>
         </div>
       </div>
