a2d937d07b871c67477a10bfec323e8e6fc8718e
niponpal <niponpal52@gmail.com>
2026-07-09 19:45:55 +0600

    send messages data

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -50,7 +50,7 @@ const Contact = () => {
             type="submit"
             className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
           >
-            Send Message Here
+            Send Message
           </button>
         </form>
 
