24fba84328b1b8759150553b212a0c0fa38a72d3
niponpal <niponpal52@gmail.com>
2026-07-10 13:29:55 +0600

    rewrite login page

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -51,7 +51,7 @@ const Login = () => {
           type="submit"
           className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
         >
-          Login Here
+          Login
         </button>
       </form>
     </div>
