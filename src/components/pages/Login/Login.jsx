953e4a3aea0ed2e5928e361942609c84157c7b1a
niponpal <niponpal52@gmail.com>
2026-07-10 13:29:21 +0600

    add extra login

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -51,7 +51,7 @@ const Login = () => {
           type="submit"
           className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
         >
-          Login
+          Login Here
         </button>
       </form>
     </div>
