47a058e6d898b2505318bd79920a8bc253f156b8
niponpal <niponpal52@gmail.com>
2026-07-13 23:48:31 +0600

    resolve register page

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -25,7 +25,7 @@ const Register = () => {
         onSubmit={handleSubmit}
         className="bg-white p-6 rounded-lg shadow-md w-80"
       >
-        <h2 className="text-2xl font-bold mb-4 text-right">Register</h2>
+        <h2 className="text-2xl font-bold mb-4 text-right">Register Here</h2>
 
         <input
           type="text"
