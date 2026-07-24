373268af97d91a7f9e54d1308293e42920db3527
niponpal <niponpal52@gmail.com>
2026-07-24 11:33:37 +0600

    change slider info

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -59,7 +59,7 @@ const Slider = () => {
         >
           <img
             src={slide.image}
-            alt={slide.title}
+            alt={slide.content}
             className="w-full h-full object-cover"
           />
 
