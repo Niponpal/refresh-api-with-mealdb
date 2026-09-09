372a48225e9cff59db750d59708da38d96857b95
niponpal <niponpal52@gmail.com>
2026-09-09 22:12:19 +0600

    refactor Course

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,7 +1,14 @@
 const Course = () => {
   return (
     <div>
-      
+      <h1>This is course component</h1>
+      <h2>Courses</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>Tailwind</li>
+      </ul>
     </div>
   );
 };
