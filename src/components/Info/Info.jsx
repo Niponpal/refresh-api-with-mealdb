82a36069618114424f7b7ebfbeea16cbf20950fa
niponpal <niponpal52@gmail.com>
2026-09-12 20:36:02 +0600

    add some dummy text for info

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -2,6 +2,7 @@ const Info = () => {
   return (
     <div>
       <h1>This is imfo Component here</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos, distinctio. Nulla dicta explicabo consectetur ipsa reprehenderit asperiores omnis blanditiis qui.</p>
     </div>
   );
 };
