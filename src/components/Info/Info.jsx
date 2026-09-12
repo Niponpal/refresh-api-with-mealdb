148a4ca9bed715f18ee87eced01432c50416d582
niponpal <niponpal52@gmail.com>
2026-09-12 20:36:46 +0600

    add button for info

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -3,6 +3,7 @@ const Info = () => {
     <div>
       <h1>This is imfo Component here</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos, distinctio. Nulla dicta explicabo consectetur ipsa reprehenderit asperiores omnis blanditiis qui.</p>
+      <button>Click More to see dummy</button>
     </div>
   );
 };
