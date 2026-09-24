a48781623c3d718fef9b316b84de9d27ce6b99ba
niponpal <niponpal52@gmail.com>
2026-09-24 21:37:47 +0600

    add something test text for private comp

--- src/components/Private/Private.jsx
+++ src/components/Private/Private.jsx
@@ -1,6 +1,7 @@
 function Private() {
     return ( <div>
         <h1>This is privabe comp</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio harum numquam nobis deserunt nulla in iusto architecto vero aliquam illo, obcaecati commodi cumque, aspernatur a error, quaerat animi vel eveniet consequatur ullam cum id! Et cum, quam natus animi aliquam ratione assumenda repellat ipsam magnam temporibus! Doloremque esse perspiciatis blanditiis?</p>
     </div> );
 }
 
